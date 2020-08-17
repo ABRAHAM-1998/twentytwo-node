65907453802e10322add7fa3fc1ee2af73a2067d
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2020-08-18 01:12:19 +0530

    4nd commit

--- twentytwo.js
+++ twentytwo.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+var cors = require('cors')
 
 const db = require('./database/dbmongo');
 const router = require('./route/login-register');
@@ -16,6 +17,7 @@ const token =require("./route/middleware");
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
+app.use(cors)
 
 
 app.use(require("./route/login-register"));
