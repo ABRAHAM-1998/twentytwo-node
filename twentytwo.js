916a676fd355eb383aa434f9f41dd2be010ff0a6
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2020-08-18 01:21:44 +0530

    4nd commit

--- twentytwo.js
+++ twentytwo.js
@@ -17,7 +17,7 @@ const token =require("./route/middleware");
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
-app.use(cors)
+app.use(cors())
 
 
 app.use(require("./route/login-register"));
